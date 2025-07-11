2d9c7beb771f560633971ca35c46f8d767b7079a
Jose Henao <josexdjose14@gmail.com>
2025-07-11 00:03:32 -0500

    last change

--- app/ui/homeCardsContainer.tsx
+++ app/ui/homeCardsContainer.tsx
@@ -7,12 +7,7 @@ interface Props {
 
 export default function CountriesTable({ countryListX }: Props) {
     return (
-        <section className="grid gap-6 p-4
-  grid-cols-1 
-  sm:grid-cols-2 
-  md:grid-cols-3 
-  xl:grid-cols-4
-  max-w-screen-xl mx-auto">
+        <section className="grid gap-6 p-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 max-w-screen-xl mx-auto">
             {countryListX && countryListX.length > 0 ? (
                 countryListX.map((country) => (
                     <HomeCard country={country} key={country.name.common} />
